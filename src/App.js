4b7b70f168bc5d652d185f8c09dfd05f47d14af8
Jon Tokugawa <79482239+Joldon@users.noreply.github.com>
2021-09-16 22:33:53 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
-import Projects from './components/Projects';
-import Header from './components/Header';
+import ProjectsList from './components/ProjectsList';
+// import Header from './components/Header';
 import About from './components/About'
+import{ Container } from 'reactstrap'
 // import Skills from './components/Skills'
 import './custom.scss'
 
+
 function App() {
   return (
-    <main className="App">
-     <h2>JOLDON KUTMANALIEV</h2>
-     <Header />
-     <About />
+    <div className='App'>
+      <header fluid>
+        <h2>JOLDON KUTMANALIEV</h2>
+      </header>
+     <main className="mb-0">
+       <About />
+       
      {/* <Skills /> */}
-     <Projects />
-    </main>
+       <Container>
+        <ProjectsList />
+       </Container>
+      </main>
+    </div>
   );
 }
 
